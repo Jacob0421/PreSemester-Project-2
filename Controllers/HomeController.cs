0a2b0ad3ca0930d7cd0f83f8cd3ecaf7241ef102
JacobRoberts <n01160799@unf.edu>
2020-08-01 14:32:31 -0400

    copy-pasted other projects fo;es

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using PreSemester_Project.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace PreSemester_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        /*
+         * 
+         * Tentative process using the site
+         * 
+         * WILL UPDATE THIS AS WE MOVE ON IN THE PROJECT:
+         * 
+         * 1.) Index (Login page) -Done
+         * 2.) Login() (Login Validation) => Landing -Done
+         * 3.) Landing Page => Add_Volunteers => AddVolunteers() OR Landing Page => Search Results Page => Edit Page (Maybe pop-up) => Landing Page
+         * 
+         * 
+         */
+
+        private readonly IVolunteerRepository _volunteerRepository;
+        
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IVolunteerRepository volunteerRepository)
+        {
+            _logger = logger;
+            _volunteerRepository = volunteerRepository;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Login(IFormCollection Form)
+        {
+            // LOGIN FORM VALIDATION IS WORKING...
+            // WILL UNCOMMENT TOWARDS END OF PROJECT
+            return RedirectToAction("ManageVolunteers");
+
+            ///// taking in login form from index.cshtml and gathering variables
+            //string username = (Form["UserName"].ToString());
+            //string password = (Form["Password"].ToString());
+
+            ////validation of "admin" credentials
+            //if (username == "Admin" && password == "Admin")
+            //{
+            //    //initializing session variables
+            //    HttpContext.Session.SetString("Username", username);
+            //    HttpContext.Session.SetString("Password", password);
+            //    return View("Landing");
+            //} 
+            //else
+            //{
+
+            //    //message returned if invalid credentials are entered
+            //    ViewBag.error = "Invalid Credentials: Please re-enter.";
+            //    return View("Index");
+            //} 
+        }
+
+        public ActionResult ManageVolunteers()
+        {
+            IEnumerable<Volunteer> volList = _volunteerRepository.GetAllVolunteers();
+
+            ViewData.Model = volList;
+            return View();
+        }
+
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Create(Volunteer newVol)
+        {
+            _volunteerRepository.Add(newVol);
+
+
+            return RedirectToAction("ManageVolunteers");
+        }
+
+        public RedirectToActionResult Delete(int id)
+        {
+            _volunteerRepository.Delete(id);
+
+            return RedirectToAction("ManageVolunteers");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Volunteer toBeChanged = _volunteerRepository.GetVolunteer(id);
+            ViewData.Model = toBeChanged;
+
+            return View();
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Edit(Volunteer changedVol)
+        {
+            _volunteerRepository.Edit(changedVol);
+
+            return RedirectToAction("ManageVolunteers");
+        }
+
+        //Search is Currently a WIP
+        [HttpGet]
+        public ActionResult Search(string key)
+        {
+
+            IEnumerable<Volunteer> results = _volunteerRepository.Search(key);
+
+            ViewData.Model = results;
+            return View("SearchResults");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+} 
---
+++ Models/IVolunteerRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PreSemester_Project.Models
+{
+    public interface IVolunteerRepository
+    {
+        Volunteer Add(Volunteer volunteer);
+        Volunteer Edit(Volunteer volunteerChanges);
+        Volunteer Delete(int id);
+        IEnumerable<Volunteer> GetAllVolunteers();
+        IEnumerable<Volunteer> Search(string key);
+        Volunteer GetVolunteer(int id);
+    }
+
+}
